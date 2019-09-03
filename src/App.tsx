14aef39b5cd1deb10fc8a722da0ee04913ac75d3
Sebastian Murray <sebastian.murray@alumnos.uv.cl>
2019-09-03 14:58:58 -0400

    Import librerias de semantic

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'semantic-ui-css/semantic.min.css'
+import { Button } from 'semantic-ui-react';
 
 const App: React.FC = () => {
   return (
